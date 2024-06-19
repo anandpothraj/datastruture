95aa61d9c5d881a06b22e281c1e4132f77baae15
anandpothraj <anandpothraj11052001@gmail.com>
2024-06-19 23:51:01 +0530

    Learned Hash table

---
+++ HashTable.js
@@ -0,0 +1,70 @@
+class HashTable{
+    constructor(size) {
+        this.table = new Array(size);
+        this.size = size;
+    }
+
+    hash(key){
+        let total = 0;
+        for(let i=0; i<key.length; i++){
+            total += key.charCodeAt(i);
+        }
+        return total % this.size;
+    }
+
+    set(key, value){
+        const index = this.hash(key);
+        let bucket= this.table[index];
+        if(!bucket){
+            this.table[index] = [[key, value]];
+        }
+        else{
+            const sameKeyItem = bucket.find(item => item[0] === key);
+            if(sameKeyItem){
+                sameKeyItem[1] = value;
+            }
+            else{
+                bucket.push([key, value]);
+            }
+        }
+    }
+
+    get(key){
+        const index = this.hash(key);
+        let bucket = this.table[index];
+        if(bucket){
+            const sameKeyItem = bucket.find(item => item[0] === key);
+            if(sameKeyItem){
+                return sameKeyItem[1];
+            }
+        }
+        return undefined;
+    }
+
+    remove(key){
+        const index = this.hash(key);
+        let bucket = this.table[index];
+        if(bucket){
+            const sameKeyItem = bucket.find(item => item[0] === key);
+            if(sameKeyItem){
+                bucket.splice(bucket.indexOf(sameKeyItem), 1);
+            }
+        }
+    }
+
+    display(){
+        for(let i=0; i<this.size; i++){
+            if(this.table[i]){
+                console.log(i, this.table[i]);
+            }
+        }
+    }
+}
+
+let table = new HashTable(50);
+table.set("name", "Anand");
+table.set("mane", "Pothraj");
+table.set("name", "Ashwini");
+table.set("age", 23);
+table.display();
+console.log(table.get("age"));
